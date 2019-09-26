517616ccac6aa1fa0c4531aefc349e2e26736cba
Nicholas Walton <cactar65@gmail.com>
2019-09-26 20:55:54 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -7,6 +7,6 @@ sum =  second_number + first_number
 
 difference = first_number - second_number
 
-product = first_number / second_number
+product = first_number * second_number
 
-quotient = first_number * second_number
+quotient = first_number / second_number
